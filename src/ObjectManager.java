cc1107135b8425818c6d28a8e21edbbefd271487
zzmimicoder <47702293+zzmimicoder@users.noreply.github.com>
2019-03-23 15:58:46 -0700

    almost done with the game just need to add game features.

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -6,48 +6,53 @@ import java.util.Random;
 
 public class ObjectManager {
 	Segment head;
-	Segment head2;
+	int blueScore = 0;
+	int redScore = 0;
 	ArrayList<Segment> tailArray = new ArrayList<Segment>();
 	String direction = "down";
 	int lives = 14;
-	String direction2= "down2";
+	String direction2 = "down2";
 	int foodW = 40;
-	 int foodL = 40;
-	 Random rand = new Random();
-		int xLocation = rand.nextInt(801-40);
-		int yLocation = rand.nextInt(801-40);
-		Random rand2 = new Random();
-		int xLocation2 = rand2.nextInt(801-40);
-		int yLocation2 = rand2.nextInt(801-40);
-		int scoreP1=0;
-		int scoreP2=0;
-		ObjectManager() {
-		head = new Segment(50, 50, 30, 30);
-		head2 = new Segment(50,50,30,30);
-		
+	int foodL = 40;
+	Random rand = new Random();
+	int xLocation = rand.nextInt(801 - 40);
+	int yLocation = rand.nextInt(801 - 40);
+	Random rand2 = new Random();
+	int xLocation2 = rand2.nextInt(801 - 40);
+	int yLocation2 = rand2.nextInt(801 - 40);
+	int scoreP1 = 0;
+	int scoreP2 = 0;
+	int headWidth = 30;
+	int headLenght = 30;
+
+	ObjectManager() {
+		head = new Segment(50, 50, headWidth, headLenght);
+
 	}
 
 	public void setdirection(String value) {
 		direction = value;
-		
+
 	}
+
 	public void setdirection2(String value2) {
 		direction2 = value2;
-		
+
 	}
-	
+
 	public String getdirection() {
 		return this.direction;
 	}
+
 	public String getdirection2() {
 		return this.direction2;
 	}
+
 	void update() {
 		move();
 		head.update();
-		head2.update();
 		manageTail();
-		
+		checkCollision();
 	}
 
 	void draw(Graphics graphics) {
@@ -60,42 +65,41 @@ public class ObjectManager {
 	}
 
 	void move() {
-		int vel=20;
-		int xVel=0;
-		int yVel=0;
+		int vel = 20;
+		int xVel = 0;
+		int yVel = 0;
 		switch (direction) {
 		case "up":
-			yVel-=vel;
+			yVel -= vel;
 			break;
 		case "down":
-			yVel+=vel;
+			yVel += vel;
 			break;
 		case "left":
-			xVel-=vel;
+			xVel -= vel;
 			break;
 		case "right":
-			xVel+=vel;
+			xVel += vel;
 			break;
 		}
 		switch (direction2) {
 		case "up":
-			yVel-=vel;
+			yVel -= vel;
 			break;
 		case "down":
-			yVel+=vel;
+			yVel += vel;
 			break;
 		case "left":
-			xVel-=vel;
+			xVel -= vel;
 			break;
 		case "right":
-			xVel+=vel;
+			xVel += vel;
 			break;
 		}
-	head.setX(head.getX()+xVel);
-	head.setY(head.getY()+yVel);
+		head.setX(head.getX() + xVel);
+		head.setY(head.getY() + yVel);
 	}
-	
-	
+
 	void drawTail(Graphics g) {
 		// Draw a 10 by 10 rectangle for each Segment in your snake ArrayList.
 		for (Segment ta : tailArray) {
@@ -103,15 +107,17 @@ public class ObjectManager {
 		}
 
 	}
+
 	void drawFood(Graphics g) {
 		g.setColor(Color.RED);
 		g.fillRect(xLocation, yLocation, foodL, foodW);
 	}
+
 	void drawFood2(Graphics g) {
 		g.setColor(Color.BLUE);
 		g.fillRect(xLocation2, yLocation2, foodL, foodW);
 	}
-	
+
 	void manageTail() {
 
 		tailArray.add(new Segment(head.getX(), head.getY(), 30, 30));
@@ -130,20 +136,31 @@ public class ObjectManager {
 	}
 
 	void checkCollision() {
-		
-		 if (xLocation==head.x && yLocation==head.y) {
-			scoreP1++;
-			 xLocation = rand2.nextInt(801-40);
-			 yLocation = rand2.nextInt(801-40);
+
+		if (head.x < xLocation + foodW && head.x > xLocation- headWidth && head.y < yLocation + foodL
+				&& head.y > yLocation - headLenght) {
+			redScore++;
+			xLocation = rand.nextInt(801 - 40);
+			yLocation = rand.nextInt(801 - 40);
+
 		}
-		 if (xLocation2==head.x && yLocation2==head.y) {
-				scoreP2++;
-				 xLocation2 = rand2.nextInt(801-40);
-				 yLocation2 = rand2.nextInt(801-40);
+
+		if (head.x < xLocation2 + foodW && head.x > xLocation2- headWidth && head.y < yLocation2 + foodL && head.y > yLocation2 - headLenght) {
+			blueScore++;
+			xLocation2 = rand2.nextInt(801 - 40);
+			yLocation2 = rand2.nextInt(801 - 40);
+
+		}
+
 	}
+
+	public int getScoreBlue() {
+		return blueScore;
+
 	}
-	// public int getscore() {
-	// return score;
-	// }
+
+	public int getScoreRed() {
+		return redScore;
+	}
 
 }
